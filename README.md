7344ceebc0a1c673b2fd82797db83e5acdcc3583
O. R. Lissenberg <olissenberg@gmail.com>
2014-04-11 23:15:48 +0200

    Laravel 4 Provider & Documentation

--- README.md
+++ README.md
@@ -1,3 +1,29 @@
 ##Zend Server 6 Job Queue
 
-http://files.zend.com/help/Zend-Server-6/content/jobs_component.htm
+[Documentation](http://files.zend.com/help/Zend-Server-6/content/jobs_component.htm)
+
+Add the Service Provider to your config/app.php
+
+    'Olissenberg\Queue\ZendJobQueueServiceProvider',
+
+Add configuration to the config/queue.php
+
+    'zendjobqueue' => [
+        'driver' => 'zendserver',
+        'options' => [],
+        'callback-url' => '/queue/zendserver',
+    ],
+
+Should work just like Iron IO push queues from that point on, note that it only works with
+paid versions of Zend Server.
+
+Notes and ToDo's
+
+- Don't use large payloads, performance will suffer (see documentation).
+- Zend Server 6.3 has https support, we had issues with earlier versions.
+- The timed/delayed jobs are not implemented yet.
+- Probably 75% copied/reused from IronQueue ...
+- ToDo: Tests!
+
+MIT License, enjoy.
+
--- src/Orlissenberg/Queue/ZendJobQueueServiceProvider.php
+++ src/Orlissenberg/Queue/ZendJobQueueServiceProvider.php
@@ -1,6 +1,7 @@
-<?php namespace Olissenberg\Queue;
+<?php namespace Orlissenberg\Queue;
 
 use Illuminate\Support\ServiceProvider;
+use Orlissenberg\Queue\ZendJobQueue;
 
 class ZendJobQueueServiceProvider extends ServiceProvider
 {
@@ -11,8 +12,44 @@ class ZendJobQueueServiceProvider extends ServiceProvider
         $this->package('orlissenberg/laravel-zendserver-pushqueue');
 
         // 1. Need a route to marshal requests.
+        \Route::any(
+            "/queue/zendserver",
+            function () {
+                /** @var ZendJobQueue $connection */
+                $connection = \Queue::connection("zendjobqueue");
+                $connection->marshal();
+            }
+        );
+
         // 2. Add the connector to the Queue facade.
+        \Queue::addConnector(
+            "zendserver",
+            function () {
+                return app()->make("\\Orlissenberg\\Queue\\Connectors\\ZendJobQueueConnector");
+            }
+        );
+
         // 3. Add configuration to the app/config/queue.php
+        /*
+        'zendjob' => [
+            'driver' => 'zendserver',
+            'options' => [],
+            'callback-url' => '/queue/zendserver',
+        ],
+        */
+
+        // 4. Enable the test route (optional for testing only)
+        /*
+        \Route::get(
+            "/queue/zendtest",
+            function () {
+                $connection = \Queue::connection("zendjobqueue");
+                $connection->push("\\Orlissenberg\\Queue\\Handlers\\TestHandler@handle", ["laravel4" => "rocks"]);
+
+                return "Job queued.";
+            }
+        );
+        */
     }
 
     /**
@@ -22,6 +59,6 @@ class ZendJobQueueServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        // TODO: Implement register() method.
+        // ...
     }
 }
